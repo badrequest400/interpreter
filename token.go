3885e399f4122b1f214a7ec733f838688cd7dd78
Balazs Cseh <balazs.cseh@arcadiagroup.co.uk>
2017-03-24 10:59:54 +0000

    just saving

---
+++ token.go
@@ -0,0 +1,28 @@
+package token
+
+type Token struct {
+  Type string
+  Literal string
+}
+
+const (
+  ILLEGAL = "ILLEGAL"
+  EOF = "EOF"
+
+  IDENT = "IDENT"
+  INT = "INT"
+
+  ASSIGN = "="
+  PLUS = "+"
+
+  COMMA = ","
+  SEMICOLON = ";"
+
+  LPAREN = "("
+  RPAREN = ")"
+  LBRACE = "{"
+  RBRACE = "}"
+
+  FUNCTION = "FUNCTION"
+  LET = "LET"
+)
